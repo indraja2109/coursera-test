e1e81895b7b1d068e689fad526e8b330e1e57e69
indraja2109 <65655220+indraja2109@users.noreply.github.com>
2020-08-21 17:33:13 +0530

    Add files via upload

---
+++ Week 4/js/script.js
@@ -0,0 +1,12 @@
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for(name in names){
+  var firstLetter = names[name].charAt(0);
+  if(firstLetter == 'j' || firstLetter == 'J'){
+    byeSpeaker.speak(names[name]);
+  }
+  else{
+    helloSpeaker.speak(names[name]);
+  }
+}
+
